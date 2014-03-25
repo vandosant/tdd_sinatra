0e67e6145c4559e4cb7b0740f3365984d2afe697
Ellie Schneiders <ems2141@gmail.com>
2014-03-25 14:01:12 -0600

    Refactored code to associate items with an id
    This was catalyzed by beginning to write the show item test
    We made enough changes in the refactoring that felt it was worth committing to continue onward with the show item spec.
    
    Signed-off-by: Scott Skender <sjskender@gmail.com>

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
+require './lib/items_repo'
 
-MENU_ITEMS = []
+#MENU_ITEMS = []
+ITEMS_REPO = ItemsRepo.new
 
 class App < Sinatra::Base
   get '/' do
@@ -11,9 +13,15 @@ class App < Sinatra::Base
     erb :new_item
   end
 
-  post '/' do
-    MENU_ITEMS << params[:item_name]
-    erb :index
+  get '/items' do
+    erb :items
+  end
+
+  post '/items' do
+    #MENU_ITEMS << params[:item_name]
+    @item_name = params[:item_name]
+    ITEMS_REPO.add(@item_name)
+    erb :items
   end
 
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -5,20 +5,34 @@ require_relative '../app'
 Capybara.app = App
 
 feature "item management" do
+  before do
+    visit '/'
+    click_on "Create Item"
+    fill_in "item_name", with: "Pizza"
+    click_on "Submit Item"
+  end
+
+  #user can create item
   scenario "creating an item" do
     visit '/'
 
-   expect(page).to have_title "Scott and Ellie's Food!"
-
-   #user can add an item
     click_on "Create Item"
     fill_in "item_name", with: "Chocolate"
     click_on "Submit Item"
 
-    click_on "Create Item"
-    fill_in "item_name", with: "Pizza"
-    click_on "Submit Item"
-
-    expect(page).to have_content "Chocolate Pizza"
+    expect(page).to have_content "Pizza Chocolate"
   end
+
+  #user can click "show item" and this opens a page with URL /item/:id
+  scenario "showing an item" do
+  pending
+    visit '/items'
+
+    click_on "Show Item"
+    # click on "show item" next to item1, we should route to /item/1
+    # write test to reference the value of a hash, where the URL contains the key
+    expect(page).to have_content "Pizza"
+
+  end
+
 end
