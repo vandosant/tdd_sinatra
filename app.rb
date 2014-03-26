e7bbe91d63b025d80031552f753973bd896501fd
Scott Skender <sjskender@gmail.com>
2014-03-26 16:55:19 -0600

    Refactored app file to stop using instance variables

--- app.rb
+++ app.rb
@@ -17,33 +17,33 @@ class App < Sinatra::Application
   end
 
   post '/items' do
-    @item_name = params[:item_name]
-    ITEMS_REPO.add(@item_name)
-    erb :items
+    item_name = params[:item_name]
+    ITEMS_REPO.add(item_name)
+    redirect '/items'
   end
 
   get '/items/:id' do
-    @id = params[:id]
-    erb :id
+    id = params[:id]
+    erb :id, locals: {id: id}
   end
 
   get '/items/:id/edit' do
-    @id = params[:id]
-    erb :edit
+    id = params[:id]
+    erb :edit, locals: {id: id}
   end
 
   put '/items' do
-    @update_name = params[:update_name]
-    @id = params[:id]
-    ITEMS_REPO.items_hash.delete(@id.to_i)
-    ITEMS_REPO.items_hash[@id] = @update_name
-    erb :items
+    update_name = params[:update_name]
+    id = params[:id].to_i
+    ITEMS_REPO.items_hash.delete(id)
+    ITEMS_REPO.items_hash[id] = update_name
+    redirect '/items'
   end
 
   delete '/items' do
-    @id = params[:id]
-    ITEMS_REPO.items_hash.delete(@id.to_i)
-    erb :items
+    id = params[:id].to_i
+    ITEMS_REPO.items_hash.delete(id)
+    redirect '/items'
   end
 
 end
